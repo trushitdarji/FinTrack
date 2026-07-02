8f3f42e38f8f5f1315c480af6f951459440012c6
trushitdarji <trushitdarji55@gmail.com>
2026-07-02 17:50:18 +0530

    impliment userModel

---
+++ src/model/user.model.js
@@ -0,0 +1,36 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      trim: true,
+      minlength: [3, "Name must be at least 3 characters long."],
+      maxlength:[30,"You can enter only 15 character in username"],
+      required: true,
+    },
+    email: {
+      type: String,
+      required: [true, "email is required"],
+      unique:true,
+      lowercase: true,
+      trim: true,
+      match: [
+        /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+        "Please fill a valid email address",
+      ],
+    },
+    password: {
+      type: String,
+      required: true,
+      minlength: [6, "password must be 6 characters long"],
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+const userModel = mongoose.model("User", userSchema);
+
+export default userModel;
