d4d728dd43e4ea5ed8e823618075e5c3d68fef2a
Trushit Darji <trushitdarji55@gmail.com>
2026-08-04 21:55:16 +0530

    update Dashboard component to handle recent transactions and improve summary rendering

--- Frontend/src/pages/Dashboard.jsx
+++ Frontend/src/pages/Dashboard.jsx
@@ -2,7 +2,8 @@ import React, { useEffect, useState } from "react";
 import api from "../api/axios";
 
 const Dashboard = () => {
-  const [summary, setSummary] = useState(null);
+  const [summary, setSummary] = useState({});
+  const [recentTransactions, setRecentTransactions] = useState([]);
 
   const getDashboardSummary = async () => {
     try {
@@ -16,22 +17,45 @@ const Dashboard = () => {
     }
   };
 
+  const getRecentTransactions = async () => {
+    try {
+      const response = await api.get("/dashboard/recent");
+
+      if (response.data.success) {
+        setRecentTransactions(response.data.recentTransaction);
+      }
+    } catch (err) {
+      console.log(err.response?.data?.message);
+    }
+  };
+
   useEffect(() => {
     getDashboardSummary();
+    getRecentTransactions();
   }, []);
 
   return (
     <div>
       <h1>Dashboard</h1>
 
-      {summary && (
-        <>
-          <h2>Total Income: ₹{summary.income}</h2>
-          <h2>Total Expense: ₹{summary.expense}</h2>
-          <h2>Current Balance: ₹{summary.balance}</h2>
-          <h2>Total Transactions: {summary.totalTransactionLen}</h2>
-        </>
-      )}
+      <h2>Income: ₹{summary.income}</h2>
+      <h2>Expense: ₹{summary.expense}</h2>
+      <h2>Balance: ₹{summary.balance}</h2>
+      <h2>Total Transactions: {summary.totalTransactionLen}</h2>
+
+      <hr />
+
+      <h2>Recent Transactions</h2>
+
+      {recentTransactions.map((transaction) => (
+        <div key={transaction._id}>
+          <p>Title: {transaction.title}</p>
+          <p>Amount: ₹{transaction.amount}</p>
+          <p>Type: {transaction.type}</p>
+          <p>Category: {transaction.category}</p>
+          <hr />
+        </div>
+      ))}
     </div>
   );
 };
